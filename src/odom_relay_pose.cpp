04992d1787f080dee1786273eef3e7beb11f64c0
FLIGHT <marcelino.malmeidan.pensa@gmail.com>
2019-08-19 16:43:03 -0500

    add realsense to vislam launch file

---
+++ src/odom_relay_pose.cpp
@@ -0,0 +1,29 @@
+
+// ROS
+#include "ros/ros.h"
+
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/PoseStamped.h"
+
+ros::Publisher pose_pub;
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+  geometry_msgs::PoseStamped pose;
+  pose.header = msg->header;
+  pose.header.frame_id = "vislam";
+  pose.pose.position = msg->pose.pose.position;
+  pose.pose.orientation = msg->pose.pose.orientation;
+  pose_pub.publish(pose);
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "odom_relay_pose");
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("/camera/odom/sample_low_freq", 5, odomCallback);
+  pose_pub = n.advertise<geometry_msgs::PoseStamped>("/vislam/pose", 5);
+  
+  ros::spin();
+
+  return 0;
+} 
